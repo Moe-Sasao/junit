6fd24c49550d8a99f5067dbdd03f56134afe2676
Moe Sasao <moe.sasao@rakus-partners.co.jp>
2022-11-14 14:36:47 +0900

    2-3,2-4 done

--- src/test/java/junit/tutorial/ex02/e03/RangeTest.java
+++ src/test/java/junit/tutorial/ex02/e03/RangeTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -20,18 +21,65 @@ class RangeTest {
 	static void tearDownAfterClass() throws Exception {
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
+	Range range;
+
+	@Nested
+	class test1 {
+
+		@BeforeEach
+		void setUp() throws Exception {
+			range = new Range(0d, 10.5);
+		}
+
+		@Nested
+		class test1_1 {
+
+			@ParameterizedTest
+			@ValueSource(doubles = { 0.0, 10.5 })
+			void test1_1_1(double d) {
+				assertTrue(range.contains(d));
+			}
+
+		}
+
+		@Nested
+		class test1_2 {
+
+			@ParameterizedTest
+			@ValueSource(doubles = { -0.1, 10.6 })
+			void test1_2_1(double d) {
+				assertFalse(range.contains(d));
+			}
+
+		}
+
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+	@Nested
+	class test2 {
 
-	@ParameterizedTest
-	@ValueSource
-	void test() {
-		
+		@BeforeEach
+		void setUp() throws Exception {
+			range = new Range(-5.1, 5.1);
+		}
+
+		@Nested
+		class test2_1 {
+			@ParameterizedTest
+			@ValueSource(doubles = { -5.1, 5.1 })
+			void test2_1_1(double d) {
+				assertTrue(range.contains(d));
+			}
+		}
+
+		@Nested
+		class test2_2 {
+			@ParameterizedTest
+			@ValueSource(doubles = { -5.2, 5.2 })
+			void test2_2_1(double d) {
+				assertFalse(range.contains(d));
+			}
+		}
 	}
 
 }
--- src/test/java/junit/tutorial/ex02/e04/ConsumptionTaxTest.java
+++ src/test/java/junit/tutorial/ex02/e04/ConsumptionTaxTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -28,11 +29,49 @@ class ConsumptionTaxTest {
 	void tearDown() throws Exception {
 	}
 
-	@ParameterizedTest
-	@CsvSource({"5,100","5,3000","10,50","5,50","3,50"})
-	void test(int rate,int price) {
-		ConsumptionTax tax = new ConsumptionTax(rate);
-		
+	ConsumptionTax tax;
+
+	@Nested
+	class tax5 {
+		@BeforeEach
+		void setUp() throws Exception {
+			tax = new ConsumptionTax(5);
+		}
+
+		@ParameterizedTest
+		@CsvSource({ "100,105", "3000,3150", "50,52" })
+		void test1(int price, int inTax) {
+			assertEquals(inTax, tax.apply(price));
+		}
+
+	}
+
+	@Nested
+	class tax10 {
+		@BeforeEach
+		void setUp() throws Exception {
+			tax = new ConsumptionTax(10);
+		}
+
+		@ParameterizedTest
+		@CsvSource({ "50,55" })
+		void test2(int price, int inTax) {
+			assertEquals(inTax, tax.apply(price));
+		}
+	}
+
+	@Nested
+	class tax3 {
+		@BeforeEach
+		void setUp() throws Exception {
+			tax = new ConsumptionTax(3);
+		}
+
+		@ParameterizedTest
+		@CsvSource({ "50,51" })
+		void test2(int price, int inTax) {
+			assertEquals(inTax, tax.apply(price));
+		}
 	}
 
 }
